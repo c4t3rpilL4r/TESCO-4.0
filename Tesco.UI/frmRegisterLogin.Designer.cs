0507b99a875c4bd2bfd9917c63297dc53c389707
Rap <Rap@DESKTOP-6ALDOJE>
2019-10-25 15:04:28 +0800

    successfully added swedish sweden localization v2

--- Tesco.UI/frmRegisterLogin.Designer.cs
+++ Tesco.UI/frmRegisterLogin.Designer.cs
@@ -32,6 +32,7 @@ namespace Tesco.UI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegisterLogin));
             this.grpRegistrationDetails = new System.Windows.Forms.GroupBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
@@ -62,6 +63,7 @@ namespace Tesco.UI
             // 
             // grpRegistrationDetails
             // 
+            resources.ApplyResources(this.grpRegistrationDetails, "grpRegistrationDetails");
             this.grpRegistrationDetails.Controls.Add(this.txtPassword);
             this.grpRegistrationDetails.Controls.Add(this.txtUsername);
             this.grpRegistrationDetails.Controls.Add(this.txtPhoneNumber);
@@ -74,200 +76,131 @@ namespace Tesco.UI
             this.grpRegistrationDetails.Controls.Add(this.lblLastName);
             this.grpRegistrationDetails.Controls.Add(this.lblFirstName);
             this.grpRegistrationDetails.Controls.Add(this.lblUserName);
-            this.grpRegistrationDetails.Location = new System.Drawing.Point(12, 12);
             this.grpRegistrationDetails.Name = "grpRegistrationDetails";
-            this.grpRegistrationDetails.Size = new System.Drawing.Size(378, 217);
-            this.grpRegistrationDetails.TabIndex = 0;
             this.grpRegistrationDetails.TabStop = false;
-            this.grpRegistrationDetails.Text = "Registration Details";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(104, 172);
+            resources.ApplyResources(this.txtPassword, "txtPassword");
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(265, 22);
-            this.txtPassword.TabIndex = 6;
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(104, 144);
+            resources.ApplyResources(this.txtUsername, "txtUsername");
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(265, 22);
-            this.txtUsername.TabIndex = 5;
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(104, 116);
-            this.txtPhoneNumber.MaxLength = 11;
+            resources.ApplyResources(this.txtPhoneNumber, "txtPhoneNumber");
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(265, 22);
-            this.txtPhoneNumber.TabIndex = 4;
             this.txtPhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhoneNumber_KeyPress);
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(103, 32);
+            resources.ApplyResources(this.txtFirstName, "txtFirstName");
             this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(265, 22);
-            this.txtFirstName.TabIndex = 0;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(103, 88);
+            resources.ApplyResources(this.txtEmail, "txtEmail");
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(265, 22);
-            this.txtEmail.TabIndex = 2;
             this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(103, 60);
+            resources.ApplyResources(this.txtLastName, "txtLastName");
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(265, 22);
-            this.txtLastName.TabIndex = 1;
             // 
             // lblPassword
             // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(28, 175);
+            resources.ApplyResources(this.lblPassword, "lblPassword");
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(70, 14);
-            this.lblPassword.TabIndex = 10;
-            this.lblPassword.Text = "Password:";
             // 
             // lblPhoneNumber
             // 
-            this.lblPhoneNumber.AutoSize = true;
-            this.lblPhoneNumber.Location = new System.Drawing.Point(0, 119);
+            resources.ApplyResources(this.lblPhoneNumber, "lblPhoneNumber");
             this.lblPhoneNumber.Name = "lblPhoneNumber";
-            this.lblPhoneNumber.Size = new System.Drawing.Size(98, 14);
-            this.lblPhoneNumber.TabIndex = 8;
-            this.lblPhoneNumber.Text = "Phone Number:";
             // 
             // lblEmail
             // 
-            this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(49, 91);
+            resources.ApplyResources(this.lblEmail, "lblEmail");
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(49, 14);
-            this.lblEmail.TabIndex = 6;
-            this.lblEmail.Text = "Email:";
             // 
             // lblLastName
             // 
-            this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(20, 63);
+            resources.ApplyResources(this.lblLastName, "lblLastName");
             this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(77, 14);
-            this.lblLastName.TabIndex = 2;
-            this.lblLastName.Text = "Last Name:";
             // 
             // lblFirstName
             // 
-            this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(13, 35);
+            resources.ApplyResources(this.lblFirstName, "lblFirstName");
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(84, 14);
-            this.lblFirstName.TabIndex = 0;
-            this.lblFirstName.Text = "First Name:";
             // 
             // lblUserName
             // 
-            this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(27, 147);
+            resources.ApplyResources(this.lblUserName, "lblUserName");
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(70, 14);
-            this.lblUserName.TabIndex = 4;
-            this.lblUserName.Text = "Username:";
             // 
             // btnLogIn
             // 
-            this.btnLogIn.Location = new System.Drawing.Point(204, 257);
+            resources.ApplyResources(this.btnLogIn, "btnLogIn");
             this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
-            this.btnLogIn.TabIndex = 19;
-            this.btnLogIn.Text = "Login";
             this.btnLogIn.UseVisualStyleBackColor = true;
             this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
             // 
             // btnRegister
             // 
-            this.btnRegister.Location = new System.Drawing.Point(102, 257);
+            resources.ApplyResources(this.btnRegister, "btnRegister");
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(96, 23);
-            this.btnRegister.TabIndex = 12;
-            this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = true;
             this.btnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
             // 
             // pnlRegister
             // 
+            resources.ApplyResources(this.pnlRegister, "pnlRegister");
             this.pnlRegister.Controls.Add(this.grpRegistrationDetails);
-            this.pnlRegister.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlRegister.Enabled = false;
-            this.pnlRegister.Location = new System.Drawing.Point(0, 0);
             this.pnlRegister.Name = "pnlRegister";
-            this.pnlRegister.Size = new System.Drawing.Size(402, 251);
-            this.pnlRegister.TabIndex = 1;
             // 
             // grpLogin
             // 
+            resources.ApplyResources(this.grpLogin, "grpLogin");
             this.grpLogin.Controls.Add(this.lblLoginUserName);
             this.grpLogin.Controls.Add(this.linkForgotPassword);
             this.grpLogin.Controls.Add(this.txtLoginPassword);
             this.grpLogin.Controls.Add(this.txtLoginUsername);
             this.grpLogin.Controls.Add(this.lblLoginPassword);
-            this.grpLogin.Location = new System.Drawing.Point(12, 12);
             this.grpLogin.Name = "grpLogin";
-            this.grpLogin.Size = new System.Drawing.Size(378, 217);
-            this.grpLogin.TabIndex = 1;
             this.grpLogin.TabStop = false;
-            this.grpLogin.Text = "Login Details";
             // 
             // lblLoginUserName
             // 
-            this.lblLoginUserName.AutoSize = true;
-            this.lblLoginUserName.Location = new System.Drawing.Point(15, 88);
+            resources.ApplyResources(this.lblLoginUserName, "lblLoginUserName");
             this.lblLoginUserName.Name = "lblLoginUserName";
-            this.lblLoginUserName.Size = new System.Drawing.Size(70, 14);
-            this.lblLoginUserName.TabIndex = 4;
-            this.lblLoginUserName.Text = "Username:";
             // 
             // linkForgotPassword
             // 
+            resources.ApplyResources(this.linkForgotPassword, "linkForgotPassword");
             this.linkForgotPassword.LinkColor = System.Drawing.Color.Black;
-            this.linkForgotPassword.Location = new System.Drawing.Point(228, 138);
             this.linkForgotPassword.Name = "linkForgotPassword";
-            this.linkForgotPassword.Size = new System.Drawing.Size(124, 18);
-            this.linkForgotPassword.TabIndex = 19;
             this.linkForgotPassword.TabStop = true;
-            this.linkForgotPassword.Text = "Forgot Password?";
             this.linkForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkForgotPassword_LinkClicked);
             this.linkForgotPassword.MouseEnter += new System.EventHandler(this.linkForgotPassword_MouseEnter);
             this.linkForgotPassword.MouseLeave += new System.EventHandler(this.linkForgotPassword_MouseLeave);
             // 
             // txtLoginPassword
             // 
-            this.txtLoginPassword.Location = new System.Drawing.Point(87, 113);
+            resources.ApplyResources(this.txtLoginPassword, "txtLoginPassword");
             this.txtLoginPassword.Name = "txtLoginPassword";
-            this.txtLoginPassword.Size = new System.Drawing.Size(265, 22);
-            this.txtLoginPassword.TabIndex = 18;
             // 
             // txtLoginUsername
             // 
-            this.txtLoginUsername.Location = new System.Drawing.Point(87, 85);
+            resources.ApplyResources(this.txtLoginUsername, "txtLoginUsername");
             this.txtLoginUsername.Name = "txtLoginUsername";
-            this.txtLoginUsername.Size = new System.Drawing.Size(265, 22);
-            this.txtLoginUsername.TabIndex = 17;
             // 
             // lblLoginPassword
             // 
-            this.lblLoginPassword.AutoSize = true;
-            this.lblLoginPassword.Location = new System.Drawing.Point(16, 116);
+            resources.ApplyResources(this.lblLoginPassword, "lblLoginPassword");
             this.lblLoginPassword.Name = "lblLoginPassword";
-            this.lblLoginPassword.Size = new System.Drawing.Size(70, 14);
-            this.lblLoginPassword.TabIndex = 10;
-            this.lblLoginPassword.Text = "Password:";
             // 
             // timer
             // 
@@ -275,16 +208,13 @@ namespace Tesco.UI
             // 
             // frmRegisterLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 291);
             this.Controls.Add(this.btnLogIn);
             this.Controls.Add(this.pnlRegister);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.grpLogin);
-            this.Font = new System.Drawing.Font("Consolas", 9F);
             this.Name = "frmRegisterLogin";
-            this.Text = "TESCO";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmRegisterLogin_FormClosing);
             this.Load += new System.EventHandler(this.FrmRegisterLogin_Load);
             this.grpRegistrationDetails.ResumeLayout(false);
